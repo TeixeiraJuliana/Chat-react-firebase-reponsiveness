83f61629a3728e4fa55ec5d32c42ad45a2634754
TeixeiraJuliana <teixeirajulianakellys@gmail.com>
2022-03-15 05:59:27 -0300

    geral

--- teste-chat/src/firebase.js
+++ teste-chat/src/firebase.js
@@ -3,14 +3,13 @@ import firebase from 'firebase';
 
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCgT4DxlFwGMIEjCVe7Kh-XfNkyIjxLeHE",
-    authDomain: "chatreact-f0568.firebaseapp.com",
-    databaseURL: "https://chatreact-f0568-default-rtdb.firebaseio.com",
-    projectId: "chatreact-f0568",
-    storageBucket: "chatreact-f0568.appspot.com",
-    messagingSenderId: "1077860133064",
-    appId: "1:1077860133064:web:ccf5ea3f677a08c390ddfd",
-    measurementId: "G-ZSD82NHFGP"
+  apiKey: "AIzaSyBfB8ddOqBtKZwuSOSr2JSw5ROhiDnCZa4",
+  authDomain: "teste-chat-react-hybri.firebaseapp.com",
+  projectId: "teste-chat-react-hybri",
+  storageBucket: "teste-chat-react-hybri.appspot.com",
+  messagingSenderId: "149654688601",
+  appId: "1:149654688601:web:d703c426885f29a5163248",
+  measurementId: "G-NKHK2ZW4FW"
 })
 
 const db = firebaseApp.firestore()
@@ -18,4 +17,4 @@ const db = firebaseApp.firestore()
 const auth = firebase.auth()
 export{db, auth}
 
-console.log(db)
+
